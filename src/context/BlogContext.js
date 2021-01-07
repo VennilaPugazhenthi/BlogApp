dbcebaf31f86712cc28f8b063c02d4e40a54b7dd
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2021-01-07 14:51:43 -0500

    Refactor the code and add style

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,27 +1,28 @@
-import React, { useReducer } from "react";
+import createDataContext from "./createDataContext";
 
-const BlogContext = React.createContext();
 const blogReducer = (state, action) => {
   switch (action.type) {
     case "add_blogpost":
-      return [...state, { title: `Blog Post #${state.length + 1}` }];
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 99999),
+          title: `Blog Post #${state.length + 1}`,
+        },
+      ];
     default:
       return state;
   }
 };
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-  const addBlogPosts = () => {
+const addBlogPosts = (dispatch) => {
+  return () => {
     dispatch({ type: "add_blogpost" });
   };
-
-  return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPosts }}>
-      {children}
-    </BlogContext.Provider>
-  );
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(
+  blogReducer,
+  { addBlogPosts },
+  []
+);
