7ec4cc5af8c75c2b03d2c421ff1c1a83459a7e4f
VennilaPugazhenthi <vennilapugazhenthi@gmail.com>
2021-01-10 15:59:57 -0500

    Update EditPost

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,6 +13,14 @@ const blogReducer = (state, action) => {
           content: action.payload.content,
         },
       ];
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        if (blogPost.id == action.payload.id) {
+          return action.payload;
+        } else {
+          return blogPost;
+        }
+      });
     default:
       return state;
   }
@@ -21,7 +29,9 @@ const blogReducer = (state, action) => {
 const addBlogPosts = (dispatch) => {
   return (title, content, callback) => {
     dispatch({ type: "add_blogpost", payload: { title, content } });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
 
@@ -31,8 +41,20 @@ const deleteBlogPost = (dispatch) => {
   };
 };
 
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: "edit_blogpost",
+      payload: { id, title, content },
+    });
+    if (callback) {
+      callback();
+    }
+  };
+};
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPosts, deleteBlogPost },
+  { addBlogPosts, deleteBlogPost, editBlogPost },
   [{ title: "TEST POST", content: "TEST CONTENT", id: 1 }]
 );
